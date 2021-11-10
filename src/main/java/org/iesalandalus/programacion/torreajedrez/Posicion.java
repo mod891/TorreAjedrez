b692ca68a5e516518f996f3a3e52d007aa20f07f
David Motos <skuayer91@gmail.com>
2021-11-10 21:40:13 +0100

    err ide class object jre [unbound] no funciona autogenerate

--- src/main/java/org/iesalandalus/programacion/torreajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/Posicion.java
@@ -4,6 +4,14 @@ public class Posicion {
 	private int fila;
 	private char columna;
 	
+	public Posicion() {
+	//	super();
+		
+	}
+	
+	/*
+	 
+	 */
 	public Posicion(int fila, char columna) {
 		setFila(fila);
 		setColumna(columna);
@@ -34,42 +42,7 @@ public class Posicion {
 			new IllegalArgumentException("EXCEPCION_COLUMNA_INCORRECTA");
 		}
 	}
-
-	/**
-	 * 
-	 * @param o objeto a comparar
-	 * @return true si el estado interno de o es igual al propio
-	 */
 	
-	@Override 
-	// public boolean equals(Posicion o)
-	public boolean equals(Object o) { // Object que es superclase de los objetos
-		if (o == this) return true; // si es el propio objeto, este es igual a si mismo
-		if (!(o instanceof Posicion))// si no son objetos de la misma clase devolver falso y no comparar valor interno 
-			return false; 	
-		
-		Posicion posicion = (Posicion) o;
-		return o.getFila() == getFila() // this.getFila() this.fila
-				&& o.getColumna() == getColumna();
-	
-		// https://www.geeksforgeeks.org/overriding-equals-method-in-java/
-		// https://mkyong.com/java/java-how-to-overrides-equals-and-hashcode/
-		//https://picodotdev.github.io/blog-bitix/2016/12/como-implementar-correctamente-y-por-que-los-metodos-equals-y-hashcode-de-los-objetos-java/
-		// equal compara el valor interno de los objetos, el estado de la variables de clase y devuelve true si son iguales
-	}
-	/**
-	 * 
-	 */
-	@Override
-	public int hashCode {
-		return hashCode();
-	}
-		/*
-		 En el lenguaje de programación Java un HashCode es un identificador de 32 bits
-		  que se almacena en un Hash en la instancia de la clase.
-		   Toda clase debe proveer de un método hashCode() que permite recuperar el Hash Code asignado, por defecto, por la clase Object. 
-		
-		 */
-	
-	}
 }
+
+
