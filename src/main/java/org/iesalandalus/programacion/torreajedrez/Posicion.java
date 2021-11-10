1ccab7fa22d0782d174402ba544ca6753ece02bf
David Motos <skuayer91@gmail.com>
2021-11-10 17:52:52 +0100

    setter & getters & excepcion?

--- src/main/java/org/iesalandalus/programacion/torreajedrez/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/Posicion.java
@@ -4,4 +4,30 @@ public class Posicion {
 	private int fila;
 	private char columna;
 	
+	public Posicion(int fila, char columna) {
+		setFila(fila);
+		setColumna(columna);
+	}
+	public int getFila() {
+		return fila;
+	}
+	public void setFila(int fila) {
+		if (fila < 1 || fila > 8) {			
+			this.fila = fila;
+		} else {
+			new IllegalArgumentException("EXCEPCION_FILA_INCORRECTA");
+		}
+	}
+	public char getColumna() {
+		return columna;
+	}
+	public void setColumna(char columna) {
+		int asciiVal = (int) columna;
+		if (!(asciiVal < 98 || asciiVal > 105)) {
+			this.columna = columna;
+		} else {
+			new IllegalArgumentException("EXCEPCION_COLUMNA_INCORRECTA");
+		}
+	}
+	
 }
